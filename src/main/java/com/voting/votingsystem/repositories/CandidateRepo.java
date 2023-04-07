bb59ffb29bd170132443a7c7327c8dbcd9980abd
Tetiana <t.pidobid@gmail.com>
2023-04-07 18:12:39 +0200

    add Controller

--- src/main/java/com/voting/votingsystem/repositories/CandidateRepo.java
+++ src/main/java/com/voting/votingsystem/repositories/CandidateRepo.java
@@ -3,12 +3,9 @@ package com.voting.votingsystem.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.voting.votingsystem.entity.Citizen;
+import com.voting.votingsystem.entity.Candidate;
 
 @Repository
-public interface CitizenRepo extends JpaRepository<Citizen, Integer>{
-	
-	public Citizen findByName(String name);
-
+public interface CandidateRepo extends JpaRepository<Candidate, Integer> {
 
 }
